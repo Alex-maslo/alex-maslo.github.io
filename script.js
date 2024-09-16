b9fb4716287266cbf0876e73c9e05cb2ac30d0b2
Alex-maslo <4439897@gmail.com>
2024-09-16 16:39:36 +0300

    button

---
+++ script.js
@@ -0,0 +1,6 @@
+function xx() {
+  const text = document.querySelector(".change-text"); // Выбираем элемент по классу
+
+  text.innerText = "Hello"; // Меняем текст
+  console.log(text); // Выводим элемент в консоль
+}
