1270ab31de01396b0011ae8e25838a24078ea0d1
Alex-maslo <4439897@gmail.com>
2024-09-23 12:55:16 +0300

    commit 7

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 async function fetchData() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
 
-    const ul = document.createElement("ul"); // Создаем элемент списка
-    users.forEach((user) => {
-      const li = document.createElement("li"); // Создаем элемент <li>
-      li.textContent = user.name; // Устанавливаем текст элемента <li>
-      ul.appendChild(li); // Добавляем <li> в <ul>
-    });
+        const ul = document.createElement("ul"); // Создаем элемент списка
+        users.forEach((user) => {
+            const li = document.createElement("li"); // Создаем элемент <li>
+            li.textContent = user.name; // Устанавливаем текст элемента <li>
+            ul.appendChild(li); // Добавляем <li> в <ul>
+        });
 
-    document.body.appendChild(ul); // Добавляем <ul> в тело документа
-  } catch (error) {
-    console.error("Ошибка при загрузке данных:", error);
-  }
+        document.body.appendChild(ul); // Добавляем <ul> в тело документа
+    } catch (error) {
+        console.error("Ошибка при загрузке данных:", error);
+    }
 }
+
 
 fetchData();
