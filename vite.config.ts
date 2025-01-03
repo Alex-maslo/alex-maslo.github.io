0a3d0d0eec5f36e1b968df3a611b11efcda4c397
Olexandr <4439897@gmail.com>
2025-01-03 23:46:24 +0200

    Deploy React build to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Alex-maslo.github.io/", // Вкажи ім'я репозиторію
   plugins: [react()],
-})
+});
